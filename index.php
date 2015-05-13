fba4e5d04eaf61a9c03ee154b0065cf6b7f04a06
nicolafiorello <nicola.fiorello@gmail.com>
2015-05-13 22:36:17 +0200

    un po' di grafica
    servono le immagini, porcaccia la puttanaccia

--- index.php
+++ index.php
@@ -75,7 +75,31 @@ include_once('loader.php');
     #COLOMBOTRAINING
     </div>
     
-   
+<div id="stepscontainer">
+    	<div id="step1" class="step">
+        	<img src="IMAGES/HOME/step1.jpg" width="100%" height="100%"/>
+            <div id="step1descr" class="stepdescr">
+            	Programma i tuoi allenamenti
+            </div>
+        </div>
+        <div id="step2" class="step">
+        	<img src="IMAGES/HOME/step2.jpg" width="100%" height="100%"/>
+            <div id="step2descr" class="stepdescr">
+            	Porta i video sempre con te
+            </div>
+        </div>
+        <div id="step3" class="step">
+        	<img src="IMAGES/HOME/step3.png" width="100%" height="100%"/>
+            <div id="step3descr" class="stepdescr">
+            	Scopri consigli e ricette per il tuo allenamento
+            </div>
+        </div>
+</div>
+<div id="videoesempio">
+<br>	QUI ALCUNI VIDEO DI ESEMPIO.<br><br><br>
+Da decidere colori e sfondi, da riempire con immagini<br><br><br> Responsive Collaudato!
+</div>
+
 </div>
 
 </body>
